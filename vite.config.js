e8ecf88b226022c430a0a29d1d974fd59b442fe9
Ryan <ryanh5011@gmail.com>
2025-07-11 22:51:02 -0300

    .
    Co-authored-by: Daniel Ferraz <dxferraz@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv('development', process.cwd(), '');
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': env
+  },
   plugins: [react()],
   base : '/avatarize/',
   server: {
